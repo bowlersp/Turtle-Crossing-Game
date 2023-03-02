f996f7d66539e5b789d32975e731ee1f0c46eceb
bowlersp <120626722+bowlersp@users.noreply.github.com>
2023-03-01 23:02:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import time
 from turtle import Screen
 from player import Player
-#from car_manager import CarManager
-#from scoreboard import Scoreboard
+from car_manager import CarManager
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +11,8 @@ screen.title("Turtle Crossing Game")
 screen.tracer(0)
 
 player = Player((0, -280))
+car_manager = CarManager()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -21,7 +23,20 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
-print("where the fuck is the screen")
+    car_manager.create_car()
+    car_manager.move_cars()
 
+    # detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.you_lose()
+
+
+    # detect turtle at finish line
+        if player.is_at_finish_line():
+            player.go_to_start()
+            car_manager.level_up()
+            scoreboard.increase_score()
 
 screen.exitonclick()
