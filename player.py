cae42187badc34008abb288c97bfdf8c7e32454f
bowlersp <120626722+bowlersp@users.noreply.github.com>
2023-03-01 23:03:14 -0600

    Update player.py

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
@@ -6,23 +7,33 @@ UP = 90
 DOWN = 270
 
 
-
 class Player(Turtle):
-    
-  def __init__(self,STARTING_POSITION):
-    super().__init__()
-    self.penup()
-    self.shape("turtle")
-    self.color("black")
-    self.shapesize(stretch_wid=1, stretch_len=1)
-    self.goto(STARTING_POSITION)
 
-  def go_up(self):
-    self.setheading(UP)
-    new_y = self.ycor() + 20
-    self.goto(self.xcor(), new_y)
+    def __init__(self, STARTING_POSITION):
+        super().__init__()
+        self.penup()
+        self.shape("turtle")
+        self.color("green")
+        self.setheading(90)
+        self.shapesize(stretch_wid=1, stretch_len=1)
+        self.go_to_start()
 
-  def go_down(self):
-    self.setheading(DOWN)
-    new_y = self.ycor() - 20
-    self.goto(self.xcor(), new_y)
+    def go_up(self):
+        self.setheading(90)
+        self.forward(MOVE_DISTANCE)
+
+    def go_down(self):
+        self.setheading(270)
+        self.forward(MOVE_DISTANCE)
+        #self.setheading(DOWN)
+        #new_y = self.ycor() - 10
+        #self.goto(self.xcor(), new_y)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            pass
